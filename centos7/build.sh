0126ba9b3f6e77156b429a907c0bc54d266de49a
Michael Nikitochkin <miry.sof@gmail.com>
2016-10-02 11:25:34 +0200

    guess where is vmware

--- centos7/build.sh
+++ centos7/build.sh
@@ -10,4 +10,10 @@ echo
 
 cat http/ks.cfg.sample | replace \$USER_KEY "$(cat ~/.ssh/id_rsa.pub)" | replace \$ROOT_PASSWORD "${root_password}" | replace \$USER_PASSWORD "${user_password}" > http/ks.cfg
 
-packer build -var centos_password="$user_password" -var fusion_app_path="$HOME/Applications/VMware Fusion.app" centos7.json
+fusion_app_path="/Applications/VMware Fusion.app"
+if [ -d $HOME/Applications/VMware\ Fusion.app ]
+then
+  fusion_app_path="$HOME/Applications/VMware Fusion.app"
+fi
+
+packer build -var centos_password="$user_password" -var fusion_app_path=${fusion_app_path} centos7.json
